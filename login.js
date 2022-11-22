bffbd951a0f6e4816a65e6ac528da9c8925e1eaf
Umgoel <76695806+Umgoel@users.noreply.github.com>
2022-11-22 22:06:24 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,30 @@
+function validation()
+{
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+
+    var pattern = /^[^ ]+@bmsce.ac.in$/;
+
+    if(email.match(pattern))
+    {
+        // form.classList.add("valid");
+        // form.classList.remove("invalid");
+        text.innerHTML = "Your Email Address in valid.";
+        text.style.color = "#00ff00";
+    }
+    else{
+        // form.classList.remove("valid");
+        // form.classList.add("invalid");
+        text.innerHTML = "Please Enter valid Email Address";
+        text.style.color = "#ff0000"
+    }
+
+    if(email == "")
+    {
+        // form.classList.remove("valid");
+        // form.classList.remove("invalid");
+        text.innerHTML = " ";
+        text.style.color = "#00ff00";
+    }
+}
